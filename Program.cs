88a7da01eae22aa53b466a4aa650825fb432a901
Emre AKA <aka548548@gmail.com>
2022-11-11 09:55:47 +0300

    MudBlazor

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using PLC_Simulation;
 using PLC_Simulation.Services;
 
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<SimulationService>();
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
